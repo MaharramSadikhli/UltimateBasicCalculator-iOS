6f02c76b38a8edfbc2d9717fab017fd1c54d8108
maharram sadikhli <s.muarem10@gmail.com>
2024-07-08 13:32:07 +0400

    created ui

--- UltimateBasicCalculator-iOS/ViewController.swift
+++ UltimateBasicCalculator-iOS/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumText: UITextField!
+    
+    @IBOutlet weak var secondNumText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func calculateDotProduct(_ sender: Any) {
+    }
+    
+    
+    @IBAction func calculateDivide(_ sender: Any) {
+    }
+    
+    
+    @IBAction func calculateAddition(_ sender: Any) {
+    }
+    
+    
+    @IBAction func calculateDifference(_ sender: Any) {
+    }
+    
 }
 
