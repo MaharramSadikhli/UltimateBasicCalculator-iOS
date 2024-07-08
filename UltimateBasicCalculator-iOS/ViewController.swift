6009f993d35cf968b7797ddff4164980b8b9a5d3
maharram sadikhli <s.muarem10@gmail.com>
2024-07-08 13:12:30 +0400

    Initial Commit

---
+++ UltimateBasicCalculator-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UltimateBasicCalculator-iOS
+//
+//  Created by Maharram SADIKHLI on 8.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
